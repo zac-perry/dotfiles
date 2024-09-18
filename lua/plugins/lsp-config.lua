05a4f69a3a9d4fc3272c12b85bedcd38f4b6b695
zperry4 <zactperry73@gmail.com>
2024-09-17 21:59:35 -0400

    add clangd setup for completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -35,7 +35,9 @@ return {
       lspconfig.gopls.setup({
         capabilities = capabilities,
       })
-
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
