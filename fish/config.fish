76b0435ade8849c501941435018f6bbeedd38b62
zperry4 <zactperry73@gmail.com>
2025-03-18 20:10:52 -0400

    add fish conf

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+source /Users/zacperry/.docker/init-fish.sh || true # Added by Docker Desktop
+
+#set -g fish_autosuggestion_enabled 0
+set -U fish_greeting
+
+# NOTE: Running my own startup script
+bash ~/Scripts/welcome.sh
