b506017a28aae56666f140b6fc2ca119d87ea882
zperry4 <zactperry73@gmail.com>
2025-07-13 17:26:58 -0500

    dotnet lsp conf

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,56 +1,66 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    after = { "mason.nvim", "nvim-lspconfig" },
-    config = function()
-      require("mason-lspconfig").setup({
-        -- Install the following lsp servers for different languages
-        -- (commented out rn, should just auto install for each language.
-        --				ensure_installed = { "lua_ls", "gopls" },
-        automatic_enable = false,
-        ensure_installed = { "lua_ls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		dependencies = { "mason.nvim", "cmp-nvim-lsp" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      -- Config to setup the lsps
-      -- Need to set capabilities in order for cmp_nvim_lsp to work
-      -- TODO: Reconfig this so I don't need to call "setup" on everything I wanna use
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gleam.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			-- Define on_attach function
+			local on_attach = function(client, bufnr)
+				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
+				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, bufopts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+				vim.keymap.set("n", "<leader>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, bufopts)
+			end
+
+			-- Setup LSP servers
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.gleam.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				cmd_env = {
+          -- NOTE: Update this config when porting to Linux machine
+					DOTNET_ROOT = "/opt/homebrew/Cellar/dotnet/9.0.7/libexec",
+					PATH = vim.env.PATH .. ":/opt/homebrew/bin",
+				},
+				init_options = {
+					AutomaticWorkspaceInit = true,
+				},
+			})
+		end,
+	},
 }
