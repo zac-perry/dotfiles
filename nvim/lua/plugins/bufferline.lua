69c467421d53d376b23dc71f3ee61260d835dd1c
zac-perry <zactperry73@gmail.com>
2026-01-01 16:59:34 -0600

    Update nvim config (bufferline transparent fix, use vim.lsp.config, add autopairs plugin)

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,69 +1,94 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    vim.opt.termguicolors = true
-    vim.opt.mousemoveevent = true
-    
-    require("bufferline").setup({
-      options = {
-        hover = {
-          enabled = true,
-          delay = 50,
-          reveal = { "close" },
-        },
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "File Explorer",
-            text_align = "center",
-            separator = true,
-          },
-        },
-        diagnostics = "nvim_lsp",
-        modified_icon = "●",
-        separator_style = "thin",
-      },
-    })
-    
-    -- Target the actual tabline area
-    local function make_tabline_transparent()
-      -- These control the actual line/area where bufferline sits
-      vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE" })
-      vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
-      vim.api.nvim_set_hl(0, "TabLineSel", { bg = "NONE" })
-      
-      -- Also clear any bufferline backgrounds just in case
-      local groups_to_clear = {
-        "BufferLineFill",
-        "BufferLineBackground", 
-        "BufferLineTab",
-        "BufferLineTabSelected",
-        "BufferLineBuffer",
-        "BufferLineBufferSelected",
-        "BufferLineBufferVisible",
-      }
-      
-      for _, group in ipairs(groups_to_clear) do
-        local hl = vim.api.nvim_get_hl(0, { name = group })
-        if hl then
-          hl.bg = nil
-          vim.api.nvim_set_hl(0, group, hl)
-        end
-      end
-    end
-    
-    -- Apply immediately and on colorscheme changes
-    vim.schedule(make_tabline_transparent)
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      callback = function()
-        vim.schedule(make_tabline_transparent)
-      end,
-    })
-    
-    vim.keymap.set("n", "<C-i>", ":BufferLineCycleNext<CR>", {})
-    vim.keymap.set("n", "<C-o>", ":BufferLineCyclePrev<CR>", {})
-    vim.keymap.set("n", "<C-x>", ":BufferLinePickClose<CR>", {})
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		vim.opt.mousemoveevent = true
+
+		require("bufferline").setup({
+			options = {
+				hover = {
+					enabled = true,
+					delay = 50,
+					reveal = { "close" },
+				},
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						text_align = "center",
+						separator = true,
+					},
+				},
+				diagnostics = "nvim_lsp",
+				modified_icon = "●",
+				separator_style = "thin",
+			},
+			highlights = {
+				-- Force transparent backgrounds for all states
+				fill = { bg = "NONE" },
+				background = { bg = "NONE" },
+				tab = { bg = "NONE" },
+				tab_selected = { bg = "NONE" },
+				tab_separator = { bg = "NONE" },
+				tab_separator_selected = { bg = "NONE" },
+				tab_close = { bg = "NONE" },
+				close_button = { bg = "NONE" },
+				close_button_visible = { bg = "NONE" },
+				close_button_selected = { bg = "NONE" },
+				buffer_visible = { bg = "NONE" },
+				buffer_selected = { bg = "NONE" },
+				numbers = { bg = "NONE" },
+				numbers_visible = { bg = "NONE" },
+				numbers_selected = { bg = "NONE" },
+				diagnostic = { bg = "NONE" },
+				diagnostic_visible = { bg = "NONE" },
+				diagnostic_selected = { bg = "NONE" },
+				hint = { bg = "NONE" },
+				hint_visible = { bg = "NONE" },
+				hint_selected = { bg = "NONE" },
+				hint_diagnostic = { bg = "NONE" },
+				hint_diagnostic_visible = { bg = "NONE" },
+				hint_diagnostic_selected = { bg = "NONE" },
+				info = { bg = "NONE" },
+				info_visible = { bg = "NONE" },
+				info_selected = { bg = "NONE" },
+				info_diagnostic = { bg = "NONE" },
+				info_diagnostic_visible = { bg = "NONE" },
+				info_diagnostic_selected = { bg = "NONE" },
+				warning = { bg = "NONE" },
+				warning_visible = { bg = "NONE" },
+				warning_selected = { bg = "NONE" },
+				warning_diagnostic = { bg = "NONE" },
+				warning_diagnostic_visible = { bg = "NONE" },
+				warning_diagnostic_selected = { bg = "NONE" },
+				error = { bg = "NONE" },
+				error_visible = { bg = "NONE" },
+				error_selected = { bg = "NONE" },
+				error_diagnostic = { bg = "NONE" },
+				error_diagnostic_visible = { bg = "NONE" },
+				error_diagnostic_selected = { bg = "NONE" },
+				modified = { bg = "NONE" },
+				modified_visible = { bg = "NONE" },
+				modified_selected = { bg = "NONE" },
+				duplicate_selected = { bg = "NONE" },
+				duplicate_visible = { bg = "NONE" },
+				duplicate = { bg = "NONE" },
+				separator_selected = { bg = "NONE" },
+				separator_visible = { bg = "NONE" },
+				separator = { bg = "NONE" },
+				indicator_selected = { bg = "NONE" },
+				indicator_visible = { bg = "NONE" },
+				pick_selected = { bg = "NONE" },
+				pick_visible = { bg = "NONE" },
+				pick = { bg = "NONE" },
+				offset_separator = { bg = "NONE" },
+			},
+		})
+
+		vim.keymap.set("n", "<C-o>", ":BufferLineCycleNext<CR>", {})
+		vim.keymap.set("n", "<C-i>", ":BufferLineCyclePrev<CR>", {})
+		vim.keymap.set("n", "<C-x>", ":BufferLinePickClose<CR>", {})
+	end,
 }
